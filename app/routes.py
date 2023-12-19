8521970a416f962f4069f3a1f4e4371833d1b030
[xiaoenls] <[1374109801@qq.com]>
2023-12-20 00:34:03 +0800

    表单form使用提交

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template
 
 from app import app#从app包中导入 app这个实例
+from app.froms import LoginForm
+
+from flask import render_template,flash,redirect,url_for
+
 
 #2个路由
 @app.route('/')
@@ -19,3 +23,16 @@ def index():
 		}
 		]
 	return render_template('index.html',title='home',user=user)
+
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {},remember_me={}'.format(form.username.data,form.remember_me.data))
+		return redirect(url_for('index'))
+	return render_template('login.html',title='Sign In',form=form)
+
+
+
